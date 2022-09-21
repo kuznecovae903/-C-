efef29aca0cab2bc2e3e0e1f660269c434beb58d
kuznecovae903 <kuznecovae903@gmail.com>
2022-09-21 09:28:27 +0300

    Добавлена папка Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите два числа и узнайте, является ли первое исло квдратом второго числа");
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int sqr_b = b * b;
+if (sqr_b == a)
+{
+    Console.Write($"Да, первое число {a} - квадрат второго числа {b}");
+}
+else
+{
+    Console.Write($"Нет, первое число {a} - не квадрат второго числа {b}. Квадрат числа {b} - {sqr_b}");
+}
