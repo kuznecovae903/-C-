f76e53dbf09ad3b60c83b9c77c107e54248c2d16
Ekaterina <kuznecovae903@gmail.com>
2022-09-26 21:10:15 +0300

    добавлено решение для Task26

---
+++ Task016/Program.cs
@@ -0,0 +1,42 @@
+﻿// 16. Напишите программу, которая принимает на вход два числа 
+// и проверяет, является ли одно число квадратом другого.
+//5, 25 -> да
+//-4, 16 -> да
+//25, 5 -> да
+//8,9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+CheckOnSquare(firstNum,secondNum);
+
+void CheckOnSquare(int arg1, int arg2)
+{
+  int sqr = 1;
+  if (arg1 < arg2)
+  {
+    sqr = (arg1 * arg1);
+    if (sqr == arg2)
+    {
+        Console.WriteLine($"Число {arg2} является квадратом числа {arg1}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {arg2} не является квадратом числа {arg1}");
+    }
+  }
+  
+    else
+    {
+        sqr = (arg2 * arg2);
+        if (sqr == arg1)
+        {
+            Console.WriteLine($"Число {arg1} является квадратом числа {arg2}");
+        }
+        else
+        {
+            Console.WriteLine($"Число {arg1} не является квадратом числа {arg2}");
+        }
+    }
+}
---
+++ Task26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 26: Напишите программу, которая 
+//принимает на вход число и 
+//выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+Console.Write("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+int CountDigits (int end)
+{
+    int counter = 0;
+      
+    while (end!=0)
+    //while (num < 0)
+    {
+        //num = num / 10;
+        end = end/10;
+        counter++;
+    }
+    return counter;
+}
+
+Console.Write($"Количество цифр в числе {numA} равно {CountDigits(numA)}");
