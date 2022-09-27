4aae6ca5187b1e1510939ac9be37fe2d4fa07f18
kuznecovae903@gmail.com <kuznecovae903@gmail.com>
2022-09-27 23:06:01 +0300

    Решена задача Task013!!!!!!!

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -14,20 +14,47 @@ if (number > 99 && number < 1000)
 }
 if (number > 999)
 {
-    int count = 0;
-    while (count > 999)
-    {
-        int res = (number / 10); 
-        count++;
-    }
-    int res1 = (number % 10);   
- 
-        //if (res < 1000)
     //{
-//                int res1 = (number % 10); 
+    //int count = 0;
+    // while (count > 999); 
+    //int res = (number / 10);
+    //if (res > 999)
+   // {
+        int GetSecondDigit(int res)
+        {
+            int count = 0;
+            while (res > 999) 
+            {
+                res /= 10;
+                count++;
+            }    
+            res %= 10;
+            return res;
+        }
+        //if (res > 999) ;
+        //int res = (number / 10);
+        //int count = 0;
+        //count++;
+        //(res > 999);
+        //int count = 0;
+        //count++;
+   // }
+    //while (res > 999); 
+    //int count = 0;
+    //count++;
+    //{
+    //        int res = (number / 10); 
+    //      count++;
+    // }
+    //int res1 = (number % 10);
+
+    //if (res < 1000)
+    //{
+    //                int res1 = (number % 10); 
     //}
-//    }
-    Console.Write($"Третья цифра числа: {res1}");
-    
+    //    }
+    //Console.Write($"Третья цифра числа: {res1}");
+    Console.Write($"Третья цифра числа: {GetSecondDigit(number)}");
+
 }
 if (number <= 99) Console.Write("Третьей цифры нет");
