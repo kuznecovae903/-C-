d8fdc44b6145776fde3eaa8cc6a37dfc7ec4f913
kuznecovae903@gmail.com <kuznecovae903@gmail.com>
2022-09-29 11:37:12 +0300

    дополнено решение Task022

--- Task022/Program.cs
+++ Task022/Program.cs
@@ -3,18 +3,39 @@
 //5 -> 1, 4, 9, 16, 25.
 //2 -> 1,4
 
-Console.WriteLine("Введите число: ");
-int numX = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите число: ");
+//int numX = Convert.ToInt32(Console.ReadLine());
 
-int count = 1;
+//int count = 1;
 
-string StringOfSqure (int num)
+//string StringOfSqure (int num)
+//{
+    //return $"|{num} | {num*num} |";
+//}
+
+//while (count <= numX)
+//{
+  //  Console.WriteLine(StringOfSqure(count));
+    //count++;
+//}
+
+void TableSqr(int arg)
 {
-    return $"|{num} | {num*num} |";
+    int i=1;
+    if (arg > 0)
+    {
+        while (i <= arg)
+        {
+            Console.WriteLine($"{i}) {Math.Pow((i),2)}");
+            i++;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели отрицательное значение");
+    }
 }
 
-while (count <= numX)
-{
-    Console.WriteLine(StringOfSqure(count));
-    count++;
-}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+TableSqr(num);
