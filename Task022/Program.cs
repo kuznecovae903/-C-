67c53d97530775a4d4bf860cc3b8ba3ef5f3356c
kuznecovae903@gmail.com <kuznecovae903@gmail.com>
2022-09-29 11:24:08 +0300

    добавлено решение Task022

--- Task022/Program.cs
+++ Task022/Program.cs
@@ -3,4 +3,18 @@
 //5 -> 1, 4, 9, 16, 25.
 //2 -> 1,4
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число: ");
+int numX = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+string StringOfSqure (int num)
+{
+    return $"|{num} | {num*num} |";
+}
+
+while (count <= numX)
+{
+    Console.WriteLine(StringOfSqure(count));
+    count++;
+}
