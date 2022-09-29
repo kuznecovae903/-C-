72631c2b8d5e6ed4bf70d6079f1c32945998add4
kuznecovae903@gmail.com <kuznecovae903@gmail.com>
2022-09-29 10:00:36 +0300

    добавлено решение Task021

---
+++ Task020/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 20: Напишите программу, которая принимает на вход координаты двух точек и
+//находит расстояние между ними в 2D пространстве.
+//A (3,6); B (2,1) -> 5,09
+//A (7,-5); B (1,-1) -> 7,21
+
+//Код округления 
+//(функция "MidpointRoundng.ToZero" подволяет просто откинуть цифры после 2 цифры после запятой)
+//double num = 3.09987565; // 3.09
+//double numRound = Math.Round(num, 2, MidpointRounding.ToZero);
+//System.Console.WriteLine(numRound);
+
+Console.WriteLine("Введите координаты первой точки:");
+Console.WriteLine("X: ");
+int numAx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Y: ");
+int numAy = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки:");
+Console.WriteLine("X: ");
+int numBx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Y: ");
+int numBy = Convert.ToInt32(Console.ReadLine());
+
+double LenthLine (int Ax, int Ay, int Bx, int By)
+{
+if (Ax == Bx && Ay == By) return 0;
+int X = (Bx - Ax);
+int Y = (By - Ay);
+return Math.Sqrt(X*X + Y*Y);
+}
+
+double lengthAB = LenthLine(numAx,numAy,numBx,numBy); 
+
+double length = Math.Round(lengthAB,2,MidpointRounding.ToZero);
+
+
+//double numZ = Math.Sqrt(numX*numX + numY*numY);
+
+Console.WriteLine($"Расстояние между двух точек -> {length}");
+
+
+
--- Task021/Program.cs
+++ Task021/Program.cs
@@ -4,4 +4,33 @@
 //A (3,6,8); B (2,1,-7), -> 15.84
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите координаты первой точки:");
+Console.WriteLine("X: ");
+int numAx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Y: ");
+int numAy = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Z: ");
+int numAz = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки:");
+Console.WriteLine("X: ");
+int numBx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Y: ");
+int numBy = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Z: ");
+int numBz = Convert.ToInt32(Console.ReadLine());
+
+double LenthLine (int Ax, int Ay, int Az, int Bx, int By, int Bz)
+{
+if (Ax == Bx && Ay == By && Az == Bz) return 0;
+int X = (Bx - Ax);
+int Y = (By - Ay);
+int Z = (Bz - Az);
+return Math.Sqrt(X*X + Y*Y + Z*Z);
+}
+
+double lengthLine = LenthLine(numAx, numAy, numAz, numBx, numBy, numBz);
+
+double lengthAB = Math.Round(lengthLine,2,MidpointRounding.ToZero);
+
+Console.WriteLine($"Расстояние между первой и второй точкой -> {lengthAB}");
