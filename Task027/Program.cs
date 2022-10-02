127d150002d3be4d949cf78ce9a911fbe887f560
kuznecovae903 <kuznecovae903@gmail.com>
2022-10-02 21:18:26 +0300

    добавлено решение task027

--- Task027/Program.cs
+++ Task027/Program.cs
@@ -4,4 +4,21 @@
 //82 -> 10
 //9012 -> 12
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма цифр в числе {number}: {sumNumbers}");
+
+int SumNumbers (int num)
+{
+  int sum = 0;
+  if (num < 0) num = -num;
+  if (num == 0) return 0;
+  while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
