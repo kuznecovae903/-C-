cab6ef9384cba1594fa6a5b1960f02b6bab73f53
kuznecovae903@gmail.com <kuznecovae903@gmail.com>
2022-10-02 18:42:11 +0300

    добавлено решение Task024

--- Task024/Program.cs
+++ Task024/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая
+//принимает на вход число (А) и 
+//выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
 
+//1+2+3+4+5+6+7+8=
+
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNumber = SumNumber(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumber}");
+
+//метод 1 - этот метод используется когда известно количество итераций
+
+/*
+int SumNumber (int num)
+{
+  int sum = default;
+    for (int i = 0; i <=num; i++);
+    {
+        sum = sum + i;
+    }
+}
+*/
+//метод 2 - этот метод используется когда НЕ известно количество итераций
+
+int SumNumber (int num)
+{
+    int sum = default;
+    int counter = 1;
+    while (counter <= num )
+    {
+        sum += counter;
+        counter++;
+    }
+    return sum;
+}
---
+++ Task031/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] CreateArrayRndInt(int size, 
+void PrintArray(int[] array)
+int GetSumPositiveElem(int[] 
+int GetSumNegativeElem(int[] array)
+{
+    int sumNegative = default;
+
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] < 0) sumNegative += array[i];
+
+    return sumNegative;
+}
+int[] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+int sumPositive = GetSumPositiveElem(arr);
+int sumNegative = GetSumNegativeElem(arr);
+Console.WriteLine($"Сумма положительных элементов = {sumPositive}");
+Console.WriteLine($"Сумма отрицательных элементов = {sumNegative}");
