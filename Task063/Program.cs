c25bb42e4d219ca9ab179c1d64bbaa7b906c6bb8
kuznecovae903@gmail.com <kuznecovae903@gmail.com>
2022-10-19 00:07:09 +0300

    task069

---
+++ Task063/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 63: Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+void NatNumbers(int num)
+{
+    if (num == 0) return;
+    NatNumbers(num - 1);
+    Console.WriteLine($"{num} ");
+}
+
+Console.WriteLine("Введите значение N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NatNumbers(number);
